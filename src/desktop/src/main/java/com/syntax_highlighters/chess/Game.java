966f04e9f9dc0249d69fe90c792d9c4417754b97
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-03-11 23:54:33 +0100

    [Game.java] added

---
+++ src/desktop/src/main/java/com/syntax_highlighters/chess/Game.java
@@ -0,0 +1,36 @@
+package com.syntax_highlighters.chess;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Game class keeping track of the game state.
+ *
+ * Contains a Board, as well as the two players, white and black. Keeps track of
+ * which player's turn it is. Records move history. The UI uses the API of this
+ * class to interact with the game.
+ */
+public class Game {
+    private Board board;
+    private IPlayer white;
+    private IPlayer black;
+    private List<Move> history;
+
+    /**
+     * Constructor.
+     *
+     * Creates a new, blank game and sets up the board.
+     *
+     * @param white The white player
+     * @param black The black player
+     */
+    public Game(IPlayer white, IPlayer black) {
+        this.white = white;
+        this.black = black;
+        
+        this.board = new Board();
+        this.history = new ArrayList<>();
+        
+        this.board.setupNewGame(white, black);
+    }
+}
