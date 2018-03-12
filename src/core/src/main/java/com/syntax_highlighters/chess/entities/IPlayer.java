338ba916c3c74097298c1d5b1e8bc26eb257d793
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-03-12 17:18:04 +0100

    [IPlayer.java] added

---
+++ src/core/src/main/java/com/syntax_highlighters/chess/entities/IPlayer.java
@@ -0,0 +1,14 @@
+package com.syntax_highlighters.chess.entities;
+
+/**
+ * Player interface.
+ *
+ * HumanPlayer and AI player classes should implement this.
+ *
+ * NOTE: No methods yet, since I have to think about what the IPlayer interface
+ * should even be. I kinda doubt getMove would even make sense, after thinking
+ * about it a little bit.
+ */
+public interface IPlayer {
+    
+}
