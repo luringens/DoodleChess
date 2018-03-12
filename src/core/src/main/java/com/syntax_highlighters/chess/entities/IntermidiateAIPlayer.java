4ef3da184c859a1b30d2d3607286a396e01aa71f
Benjamin Dyhre Bjønnes <benjamin.bjonnes@uib.no>
2018-03-12 18:27:59 +0100

    Created classes for abstract, human and AI players

---
+++ src/core/src/main/java/com/syntax_highlighters/chess/entities/IntermidiateAIPlayer.java
@@ -0,0 +1,12 @@
+package com.syntax_highlighters.chess.entities;
+
+import com.syntax_highlighters.chess.Board;
+import com.syntax_highlighters.chess.Move;
+
+import java.util.ArrayList;
+
+class IntermediateAIPlayer extends AbstractAIPlayer {
+     void getMove(Board board, ArrayList<Move> history) {
+        return ;
+    }
+}
