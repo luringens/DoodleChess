6921ba8491e69442f52351251a0d10fd5747b07f
Robin el Salim <robin.salim@student.uib.no>
2018-03-13 14:01:25 +0100

    [Tests] Position Testing

--- src/core/test/com/syntax_highlighters/chess/coordPieceTest.java
+++ src/core/test/com/syntax_highlighters/chess/coordPieceTest.java
@@ -2,25 +2,44 @@ package com.syntax_highlighters.chess;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static com.badlogic.gdx.graphics.VertexAttributes.Usage.Position;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class coordPieceTest {
-/*    IChessPiece[][] pieces;
+   /*
     ChessPieceKing whiteKing = new ChessPieceKing(0,4);
     ChessPieceKing blackKing = new ChessPieceKing();
     ChessPieceQueen whiteQueen = new ChessPieceQueen();
-    ChessPieceQueen blackQueen = new ChessPieceQueen();
+    ChessPieceQueen blackQueen = new ChessPieceQueen();*/
     Position p = new Position(3,2);
+    String notation = "a4";
+    Position note = Position.fromChessNotation(notation);
 
 
 
     @Test
+    public void positiontest(){
+        assertEquals(p.getX(),3);
+        assertEquals(p.getY(),2);
+
+    }
+
+    @Test
+    public void parsertest(){
+        assertEquals(note.getX(),1);
+        assertEquals(note.getY(),4);
+    }
+
+    @Test
+    public void putAtPositionTest(){
+
+    }
+
+    /*
+    @Test
     public void wking(){ //checking white king pos
-        assertEquals(whiteKing.getX(),4);
-        assertEquals(whiteKing.getY(),0);
+        assertEquals(whiteKing.getX(),5);
+        assertEquals(whiteKing.getY(),1);
     }
 
     @Test
@@ -38,5 +57,6 @@ public class coordPieceTest {
     public void bQueen(){ //checking black queen pos
         assertEquals(blackQueen.getX(),3);
         assertEquals(blackQueen.getY(),7);
-    }*/
+    }
+    */
 }
