4666564ee13f394ea87648fc8c340a31edf6e7e1
Benjamin Dyhre Bjønnes <benjamin.bjonnes@uib.no>
2018-03-14 15:16:36 +0100

    Merge branch 'develop' of C:\Users\Benjamin\gruppe-6 with conflicts.

--- src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceKing.java
+++ src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceKing.java
@@ -6,91 +6,22 @@ import com.syntax_highlighters.chess.Position;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
-import java.util.ArrayList;
-public class ChessPieceKing extends AbstractChessPiece {
 
+public class ChessPieceKing extends AbstractChessPiece {
     public ChessPieceKing(Position pos, boolean isWhite) {
         super(pos, isWhite);
     }
 
-    private boolean moved = false;
-
-    public void setPieceToMoved(){
-        this.moved = true;
-    }
-
-    public boolean hasMoved(){
-        return moved;
-    }
-
-    private List<Position> allEnemyMoves(Board board){
-        List<Position> possibleMoves = new ArrayList<>();
-        for(IChessPiece a : board.getAllPieces()) {
-            if (a.isWhite() != this.isWhite()) {
-                Position piecePos = a.getPosition();
-                if(!possibleMoves.contains(piecePos)){
-                    possibleMoves.add(piecePos);
-                }
-            }
-        }
-
-        return possibleMoves;
-    }
-
     @Override
     public List<Move> allPossibleMoves(Board board) {
-        List<Move> possibleMoves = new ArrayList<>();
-        int xpos = this.getPosition().getX();
-        int ypos = this.getPosition().getY();
-        //all fields surrounding king.
-        for(int i = xpos-1; i<xpos+2; i++){
-            for(int j = ypos-1; j<ypos+2; j++){
-                Position boardPos = new Position(i,j);
-                if(board.isOccupied(boardPos)){
-                    continue;
-                }
-                if(board.getAtPosition(boardPos).isWhite() == this.isWhite()){
-                    continue;
-                }
-                if(allEnemyMoves(board).contains(boardPos)){
-                    continue;
-                }
-                possibleMoves.add(new Move(this.getPosition(), boardPos,this));
-            } 
-        }
-        //rokade
-        if(this.isWhite()) {
-            if (board.getAtPosition(new Position(1, 8)) instanceof ChessPieceRook) {
-                if (!this.hasMoved() && !((ChessPieceRook) board.getAtPosition(new Position(1, 8))).hasMoved()) {
-                    possibleMoves.add(new Move(this.getPosition(), new Position(1, 7), this));
-                }
-            }
-            if (board.getAtPosition(new Position(1, 1)) instanceof ChessPieceRook) {
-                if (!this.hasMoved() && !((ChessPieceRook) board.getAtPosition(new Position(1, 1))).hasMoved()) {
-                    possibleMoves.add(new Move(this.getPosition(), new Position(1, 3), this));
-                }
-            }
-        }else {
-            if (board.getAtPosition(new Position(8, 8)) instanceof ChessPieceRook) {
-                if (!this.hasMoved() && !((ChessPieceRook) board.getAtPosition(new Position(8, 8))).hasMoved()) {
-                    possibleMoves.add(new Move(this.getPosition(), new Position(8, 7), this));
-                }
-            }
-            if (board.getAtPosition(new Position(8, 1)) instanceof ChessPieceRook) {
-                if (!this.hasMoved() && !((ChessPieceRook) board.getAtPosition(new Position(8, 1))).hasMoved()) {
-                    possibleMoves.add(new Move(this.getPosition(), new Position(8, 3), this));
-                }
-            }
-        }
-
-        return possibleMoves;
+        throw new NotImplementedException();
     }
 
     @Override
     public IChessPiece copy() {
         throw new NotImplementedException();
     }
-    
+
     @Override
     public int getScore() {
         throw new NotImplementedException();
