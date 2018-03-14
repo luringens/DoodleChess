d5f15929a302fc707e3b350ed555e728db687712
Eirik Jørgensen <eirik.jorgensen@student.uib.no>
2018-03-14 15:39:25 +0100

    Benjamin fucked up my shit, so i fix

--- src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceBishop.java
+++ src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceBishop.java
@@ -5,6 +5,7 @@ import com.syntax_highlighters.chess.Move;
 import com.syntax_highlighters.chess.Position;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChessPieceBishop extends AbstractChessPiece {
@@ -14,7 +15,52 @@ public class ChessPieceBishop extends AbstractChessPiece {
     
     @Override
     public List<Move> allPossibleMoves(Board board) {
-        throw new NotImplementedException();
+        List<Move> possibleMoves = new ArrayList<>();
+        int xpos = this.getPosition().getX();
+        int ypos = this.getPosition().getY();
+        Position lastPos;
+        Position nextPos;
+        int i=1;
+        while (i+ypos<9 || i+xpos<9) {
+            lastPos = new Position(xpos+i-1,ypos+i-1);
+            nextPos = new Position(xpos+ i, ypos + i);
+            if (board.isEnemy(this,lastPos) || (ypos+i > 8 || xpos+i > 8) || board.isFriendly(this, nextPos))
+                break;
+            else
+                possibleMoves.add(new Move(lastPos, nextPos, this));
+            i++;
+        }
+        i=1;
+        while (i+ypos<9 || i+xpos<9) {
+            lastPos = new Position(xpos+i-1,ypos-i+1);
+            nextPos = new Position(xpos+i, ypos-i);
+            if (board.isEnemy(this,lastPos) || (ypos+i > 8 || xpos+i > 8) || board.isFriendly(this, nextPos))
+                break;
+            else
+                possibleMoves.add(new Move(lastPos, nextPos, this));
+            i++;
+        }
+        i=1;
+        while (ypos-i>0 || xpos-i>0) {
+            lastPos = new Position(xpos-i+1,ypos+i+1);
+            nextPos = new Position(xpos-i, ypos + i);
+            if (board.isEnemy(this,lastPos) || (ypos-i < 1 || xpos-i < 1) || (!board.isFriendly(this, nextPos)))
+                break;
+            else
+                possibleMoves.add(new Move(lastPos, nextPos, this));
+            i++;
+        }
+        i=1;
+        while (ypos-i>0 || xpos-i>0) {
+            lastPos = new Position(xpos-i+1,ypos-i+1);
+            nextPos = new Position(xpos-i, ypos-i);
+            if (board.isEnemy(this,lastPos) || (ypos-i < 1 || xpos-i < 1) || (!board.isFriendly(this, nextPos)))
+                break;
+            else
+                possibleMoves.add(new Move(lastPos, nextPos, this));
+            i++;
+        }
+        return possibleMoves;
     }
 
     @Override
