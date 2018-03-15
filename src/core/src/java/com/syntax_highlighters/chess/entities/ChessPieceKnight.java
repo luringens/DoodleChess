95b95b26aee1b12f63b53d548f5a2db91c430d74
Benjamin Dyhre Bjønnes <benjamin.bjonnes@uib.no>
2018-03-15 15:16:10 +0100

    [ChessPieceKnight] updated methods

--- src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceKnight.java
+++ src/core/src/java/com/syntax_highlighters/chess/entities/ChessPieceKnight.java
@@ -47,8 +47,8 @@ public class ChessPieceKnight extends AbstractChessPiece {
     }
     //Checks the move
     public void checkMove(Board board, ArrayList<Move> possibleMoves, Position pos){
-        if(board.isOnBoard(pos)){
-                if(board.getAtPosition(pos).isWhite() != this.isWhite()){ //kan også bruke isFriendly-metoden
+        if(board !=null && board.isOnBoard(pos)){
+                if(board.getAtPosition(pos) !=null && board.isEnemy(this,pos)){ //kan også bruke isFriendly-metoden
                     possibleMoves.add(new Move(this.getPosition(), pos, this));
                 }
 
