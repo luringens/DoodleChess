d931ec0597cb3af1ddbb9d1fe4cd016ba9b50238
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-03-17 16:49:54 +0100

    [AiTest.java] position pawn correctly

--- src/core/test/com/syntax_highlighters/chess/AiTest.java
+++ src/core/test/com/syntax_highlighters/chess/AiTest.java
@@ -4,6 +4,8 @@ import com.syntax_highlighters.chess.*;
 import com.syntax_highlighters.chess.entities.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 
 class AiTest {
@@ -11,12 +13,25 @@ class AiTest {
     void takesKing() {
         ArrayList<IChessPiece> pieces = new ArrayList<>();
         pieces.add(new ChessPieceKing(new Position(2, 2), false));
-        pieces.add(new ChessPiecePawn(new Position(2, 1), true));
+        pieces.add(new ChessPiecePawn(new Position(1, 1), true));
+        // Diagram:
+        //  A B C D E F G H
+        // _________________
+        // |               | 8
+        // |               | 7
+        // |               | 6
+        // |               | 5
+        // |               | 4
+        // |               | 3
+        // | K             | 2
+        // |P______________| 1
+        //
+        // Pawn will move up to (2, 2) aka B4 to capture the king
         Board board = new Board(pieces);
-
+        
         IAiPlayer ai = new MiniMaxAIPlayer(true, MiniMaxAIPlayer.Difficulty.Easy);
         ai.PerformMove(board);
 
-        assert(board.getAllPieces().size() == 1);
+        assertEquals(1, board.getAllPieces().size());
     }
 }
