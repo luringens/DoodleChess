5d749e8e9120578ea00809a1f034e51a6510cf45
Stian Soltvedt <soltvedt.stian@gmail.com>
2018-03-17 18:12:45 +0100

    [#38 AiTest] Add speed tests

--- src/core/test/com/syntax_highlighters/chess/AiTest.java
+++ src/core/test/com/syntax_highlighters/chess/AiTest.java
@@ -22,4 +22,46 @@ class AiTest {
 
         assertEquals(1, board.getAllPieces().size());
     }
+
+    @Test
+    void speedTestEasy() {
+        final long allowedTime = 1000;
+        IAiPlayer ai = new MiniMaxAIPlayer(true, MiniMaxAIPlayer.Difficulty.Easy);
+        long time = speedTest(ai);
+        System.out.print("Expected less than: " + allowedTime + ", actual: " + time);
+        assertTrue("The easy AI is too slow.", time < allowedTime);
+    }
+
+    @Test
+    void speedTestMedium() {
+        final long allowedTime = 3000;
+        IAiPlayer ai = new MiniMaxAIPlayer(true, MiniMaxAIPlayer.Difficulty.Medium);
+        long time = speedTest(ai);
+        System.out.print("Expected less than: " + allowedTime + ", actual: " + time);
+        assertTrue("The easy AI is too slow.", time < allowedTime);
+    }
+
+    @Test
+    void speedTestHard() {
+        final long allowedTime = 5000;
+        IAiPlayer ai = new MiniMaxAIPlayer(true, MiniMaxAIPlayer.Difficulty.Hard);
+        long time = speedTest(ai);
+        System.out.print("Expected less than: " + allowedTime + ", actual: " + time);
+        assertTrue("The easy AI is too slow.", time < allowedTime);
+    }
+
+    /** Measures how long an AI spends deciding a move on a fresh chess board.
+     * @param ai The AI to measure.
+     * @return The number of millseconds spent.
+     */
+    long speedTest(IAiPlayer ai) {
+        Board board = new Board();
+        board.setupNewGame();
+
+        long start = System.nanoTime();
+        ai.PerformMove(board);
+        long end = System.nanoTime();
+
+        return (end - start) / 1_000_000;
+    }
 }
