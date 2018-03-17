f9cbfae1ea86dfe26228b53eb5cc7ca36c80f8d8
theAzack9 <robin@cavachi.be>
2018-03-17 14:34:44 +0100

    [#32 MainScreen.java] Cleanup

--- src/core/src/java/com/syntax_highlighters/chess/gui/screens/MainScreen.java
+++ src/core/src/java/com/syntax_highlighters/chess/gui/screens/MainScreen.java
@@ -1,38 +1,22 @@
 package com.syntax_highlighters.chess.gui.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.syntax_highlighters.chess.Board;
 import com.syntax_highlighters.chess.Game;
-import com.syntax_highlighters.chess.Position;
-import com.syntax_highlighters.chess.entities.IChessPiece;
 import com.syntax_highlighters.chess.gui.UiBoard;
 
 /**
  * Game main screen
  */
 public class MainScreen implements Screen {
-
-    private SpriteBatch batch;
-    private OrthographicCamera camera;
-
     private Game game;
-
     private Stage stage;
-
-
     UiBoard board;
 
     public MainScreen() {
-
         game = new Game(false, false, 0);
 
         stage = new Stage(new ScreenViewport());
@@ -40,7 +24,6 @@ public class MainScreen implements Screen {
 
         board = new UiBoard(game);
         stage.addActor(board);
-
     }
 
     @Override
@@ -53,7 +36,6 @@ public class MainScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        //camera.setToOrtho(false, width, height);
         stage.getViewport().update(width, height, true);
     }
 
@@ -79,6 +61,5 @@ public class MainScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
     }
 }
