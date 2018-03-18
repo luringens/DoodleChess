fe87fd3a1735594507b0bbafc002a7537b484733
theAzack9 <robin@cavachi.be>
2018-03-18 12:09:43 +0100

    [#34 UI] Tweaks to assets and visual changes

--- src/core/src/java/com/syntax_highlighters/chess/gui/screens/MainScreen.java
+++ src/core/src/java/com/syntax_highlighters/chess/gui/screens/MainScreen.java
@@ -3,8 +3,12 @@ package com.syntax_highlighters.chess.gui.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.syntax_highlighters.chess.Game;
 import com.syntax_highlighters.chess.entities.AiDifficulty;
@@ -18,24 +22,35 @@ public class MainScreen implements Screen {
     private Stage stage;
     UiBoard board;
 
+    private Texture paper;
+    private Image paperImage;
     private AssetManager assetManager;
+
+    private SpriteBatch batch;
 
     public MainScreen(AssetManager manager) {
         assetManager = manager;
-        game = new Game(null, AiDifficulty.Easy);
+        paper = manager.get("paper.png", Texture.class);
+        paperImage = new Image(paper);
 
-        stage = new Stage(new ScreenViewport());
+        game = new Game(null, null);
+
+        batch = new SpriteBatch();
+
+        stage = new Stage(new ScreenViewport(), batch);
         Gdx.input.setInputProcessor(stage);
 
         board = new UiBoard(assetManager, game);
+        stage.addActor(paperImage);
         stage.addActor(board);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.25f, 0.3f, 0.35f, 1);
+
+        //Gdx.gl.glClearColor(0.25f, 0.3f, 0.35f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.act(delta);
         stage.draw();
     }
 
