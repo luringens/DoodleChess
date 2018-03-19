70e19fccefe631ff90df3c7793fd83b4c7ddb1e5
Stian Soltvedt <soltvedt.stian@gmail.com>
2018-03-19 11:11:05 +0100

    [BoardStartupTest] Fix KingsPos, minor style changes

--- src/core/test/com/syntax_highlighters/chess/BoardStartupTest.java
+++ src/core/test/com/syntax_highlighters/chess/BoardStartupTest.java
@@ -3,21 +3,24 @@ package syntax_highlighters.chess;
 import com.syntax_highlighters.chess.Board;
 import com.syntax_highlighters.chess.Position;
 import com.syntax_highlighters.chess.entities.*;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class BoardStartupTest {
-    Board board = new Board();
+class BoardStartupTest {
+    private Board board;
 
-    // could use @Before, if I could get it to work...
-    public void setUp() {
+    @Before
+    private void setUp() {
+        board = new Board();
         board.setupNewGame();
     }
 
     @Test
-    public void EmptySquaresAreEmpty(){
+    void EmptySquaresAreEmpty(){
         setUp();
         boolean isEmpty = true;
 
@@ -32,42 +35,36 @@ public class BoardStartupTest {
 
     }
     @Test
-    public void KingsPos(){
+    void KingsPos(){
         setUp();
-        Position wPos = new Position(4,1);
-        Position bPos = new Position(4,8);
-        boolean IsKing = true;
+        Position wPos = new Position(5,1);
+        Position bPos = new Position(5,8);
 
         // these can be null
         IChessPiece TestW = board.getAtPosition(wPos);
         IChessPiece TestB = board.getAtPosition(bPos);
 
-        // use "equals" for string comparison
-        if (TestW.getAssetName() != "King" || TestB.getAssetName() != "King"){
-            IsKing = false;
-        }
-        assertEquals(IsKing, true);
+        assertTrue(TestW instanceof ChessPieceKing);
+        assertTrue(TestB instanceof ChessPieceKing);
     }
     @Test
-    public void QueensPos(){
+    void QueensPos(){
 
     }
     @Test
-    public void PawnsPos(){
+    void PawnsPos(){
 
     }
     @Test
-    public void BishopsPos(){
+    void BishopsPos(){
 
     }
     @Test
-    public void KnightsPos(){
+    void KnightsPos(){
 
     }
     @Test
-    public void RooksPos(){
+    void RooksPos(){
 
     }
-
-
 }
