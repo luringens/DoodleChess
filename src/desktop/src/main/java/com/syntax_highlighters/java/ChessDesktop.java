2748b8e27bb7787c3a7ac8fb2e706ff18ce0e9d5
rgr015 <rgr015@uib.no>
2018-03-21 16:40:22 +0100

    [Maven] reverted to regular lwjgl

--- src/desktop/src/main/java/com/syntax_highlighters/java/ChessDesktop.java
+++ src/desktop/src/main/java/com/syntax_highlighters/java/ChessDesktop.java
@@ -1,15 +1,15 @@
 package com.syntax_highlighters.java;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.syntax_highlighters.chess.ChessGame;
 
 
 public class ChessDesktop {
 	public static void main (String[] args) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setHdpiMode(Lwjgl3ApplicationConfiguration.HdpiMode.Pixels);
-		config.setWindowedMode(800, 800);
-		new Lwjgl3Application(new ChessGame(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 800;
+		new LwjglApplication(new ChessGame(), config);
 	}
 }
