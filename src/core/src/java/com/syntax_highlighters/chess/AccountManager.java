4583efe9e70a95e99570275900bc406227e89c12
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-03-21 18:04:21 +0100

    [#41] add AccountManager class
    Class added, but no methods have been properly implemented. This is just so the
    interface exists.

---
+++ src/core/src/java/com/syntax_highlighters/chess/AccountManager.java
@@ -0,0 +1,30 @@
+package com.syntax_highlighters.chess;
+
+import java.util.List;
+
+public class AccountManager {
+    public boolean save(String filename) {
+        // TODO: implement
+        return false;
+    }
+    public boolean load(String filename) {
+        // TODO: implement
+        return false;
+    }
+    public List<Account> getAll() {
+        // TODO: implement
+        return null;
+    }
+    public List<Account> getTop(int n) {
+        // TODO: implement
+        return null;
+    }
+    public Account getAccount(String name) {
+        // TODO: implement
+        return null;
+    }
+    public boolean addAccount(Account acc) {
+        // TODO: implement
+        return false;
+    }
+}
