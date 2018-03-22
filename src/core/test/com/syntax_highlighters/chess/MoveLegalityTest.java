d2edcf4767e95e587562a8ffb77df0281f6e2951
Sverre Magnus <sen006@student.uib>
2018-03-22 20:30:16 +0100

    [Updated MoveLegalityTest.java]

--- src/core/test/com/syntax_highlighters/chess/MoveLegalityTest.java
+++ src/core/test/com/syntax_highlighters/chess/MoveLegalityTest.java
@@ -19,91 +19,81 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoveLegalityTest {
 
     @Test
-    public void HorseThreeStraightCheckMove(){
+    public void HorseThreeStraightCheckMove() {
         Board board = new Board();
 
-        Position pos = new Position(3,3);
-        Position newPos = new Position(4,5);
+        Position pos = new Position(3, 3);
+        Position newPos = new Position(3, 6);
 
-        IChessPiece testKnight = new ChessPieceKnight(pos,true);
 
-        boolean IsMoveLegal = testKnight.allPossibleMoves(board).contains(newPos);
+        IChessPiece testKnight = new ChessPieceKnight(pos, true);
+        board.putAtPosition(testKnight.getPosition(),testKnight);
+        boolean IsMoveLegal = testKnight.allPossibleMoves(board).stream().anyMatch(m -> m.getPosition().equals(newPos));
+
         System.out.println(testKnight.allPossibleMoves(board));
 
         assertFalse(IsMoveLegal);
     }
+
     @Test
-    public void WhitePawnBackwards(){
+    public void WhitePawnBackwards() {
         Board board = new Board();
 
-        Position pos = new Position(3,3);
-        Position newPos = new Position (3,2);
-        IChessPiece testPawn = new ChessPiecePawn(pos,true);
+        Position pos = new Position(3, 3);
+        Position newPos = new Position(3, 2);
+        IChessPiece testPawn = new ChessPiecePawn(pos, true);
+        board.putAtPosition(testPawn.getPosition(),testPawn);
 
-        boolean IsMoveLegal = testPawn.allPossibleMoves(board).contains(newPos);
+        boolean IsMoveLegal = testPawn.allPossibleMoves(board).stream().anyMatch(m -> m.getPosition().equals(newPos));
 
         assertFalse(IsMoveLegal);
     }
+
     @Test
-    public void BlackPawnBackwards (){
+    public void BlackPawnBackwards() {
         Board board = new Board();
 
-        Position pos = new Position(3,3);
-        Position newPos = new Position (3,4);
+        Position pos = new Position(3, 3);
+        Position newPos = new Position(3, 4);
 
-        IChessPiece testPawn = new ChessPiecePawn(pos,false);
+        IChessPiece testPawn = new ChessPiecePawn(pos, false);
+        board.putAtPosition(testPawn.getPosition(),testPawn);
 
-        boolean IsMoveLegal = testPawn.allPossibleMoves(board).contains(newPos);
+        boolean IsMoveLegal = testPawn.allPossibleMoves(board).stream().anyMatch(m -> m.getPosition().equals(newPos));
 
         assertFalse(IsMoveLegal);
     }
+
     @Test
-    public void PawnAttackEmptySquare(){
+    public void PawnAttackEmptySquare() {
         Board board = new Board();
 
-        Position pos = new Position(3,3);
-        Position newPos = new Position(4,4);
+        Position pos = new Position(3, 3);
+        Position newPos = new Position(4, 4);
 
-        IChessPiece testPawn = new ChessPiecePawn(pos,true);
+        IChessPiece testPawn = new ChessPiecePawn(pos, true);
+        board.putAtPosition(testPawn.getPosition(),testPawn);
 
-        boolean IsMoveLegal = testPawn.allPossibleMoves(board).contains(newPos);
+        boolean IsMoveLegal = testPawn.allPossibleMoves(board).stream().anyMatch(m -> m.getPosition().equals(newPos));
 
         assertFalse(IsMoveLegal);
     }
+
     @Test
     public void PieceAttackAlly() {
         Board board = new Board();
 
-        Position AtkPos = new Position(3,3);
-        Position DefPos = new Position(4,4);
+        Position AtkPos = new Position(3, 3);
+        Position DefPos = new Position(4, 4);
 
         IChessPiece testQueen = new ChessPieceQueen(AtkPos, true);
         IChessPiece testPawn = new ChessPiecePawn(DefPos, true);
+        board.putAtPosition(testQueen.getPosition(), testQueen);
+        board.putAtPosition(testPawn.getPosition(), testPawn);
 
-        boolean IsMoveLegal = testQueen.allPossibleMoves(board).contains(DefPos);
-
+        boolean IsMoveLegal = testQueen.allPossibleMoves(board).stream().anyMatch(m -> m.getPosition().equals(DefPos));
+        System.out.println(IsMoveLegal);
         assertFalse(IsMoveLegal);
     }
 
-    @Test
-    public void EnPassant () {
-        //setup board.
-        Board board = new Board();
-        board.setupNewGame();
-        IChessPiece testPawn;
-
-        //Do moves to place pawns into enpassant situation.
-        Move firstMove = new Move(new Position(6,2),new Position(6,4),board.getAtPosition(new Position(6,2)));
-        firstMove.DoMove(board);
-        Move secondMove = new Move(new Position(1,7),new Position(1,7),board.getAtPosition(new Position(1,7)));
-        secondMove.DoMove(board);
-        Move thirdMove = new Move(new Position(6,4),new Position(6,5),board.getAtPosition(new Position(6,4)));
-        thirdMove.DoMove(board);
-        Move fourthMove = new Move(new Position(6,7),new Position(6, 5),board.getAtPosition(new Position(6,7)));
-        fourthMove.DoMove(board);
-        testPawn = board.getAtPosition(new Position(6,5));
-
-        // check if the enpassant move position is among the legal moves the piece can perform.
-        assertTrue(testPawn.allPossibleMoves(board).contains(new Position(7,6)));
-    }
 }
