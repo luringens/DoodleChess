d29f3f442e8a4062574b5aa6dbaee30081313113
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-08 16:59:44 +0200

    [#69] Update JavaDoc for GameScreen constructor
    Also fixed a minor bug in GameScreen which set player1 and player2 color to the
    same color (both from attrib1)

--- src/main/java/com.syntax_highlighters.chess/gui/screens/GameScreen.java
+++ src/main/java/com.syntax_highlighters.chess/gui/screens/GameScreen.java
@@ -65,8 +65,10 @@ public class GameScreen extends AbstractScreen {
      * We have put the AI on a separate thread to stop the window from becoming unresponsive while the AI is thinking.
      *
      * @param chessGame current ChessGame
-     * @param player1Difficulty Difficulty of player 1 (null if no ai)
-     * @param player2Difficulty Difficulty of player 2 (null if no ai)
+     * @param attrib1 Attributes for player 1 (account info, AI difficulty,
+     * piece color)
+     * @param attrib2 Attributes for player 2 (account info, AI difficulty,
+     * piece color)
      */
     public GameScreen(ChessGame chessGame, PlayerAttributes attrib1, PlayerAttributes attrib2) {
         super(chessGame);
@@ -78,7 +80,7 @@ public class GameScreen extends AbstractScreen {
         this.ai1 = attrib1.getAIDifficulty();
         this.ai2 = attrib2.getAIDifficulty();
         this.player1Color = attrib1.getColor();
-        this.player2Color = attrib1.getColor();
+        this.player2Color = attrib2.getColor();
         this.chessGame = chessGame;
         
         this.game = new Game(ai1, ai2);
