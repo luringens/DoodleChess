7d97d019759c1dadd4fb6f9cf78fcd038551f35c
Eirik Jørgensen <eirik.jorgensen@student.uib.no>
2018-04-10 15:10:31 +0200

    [Account & AccountManger] fixed rating system (again).

--- src/main/java/com.syntax_highlighters.chess/AccountManager.java
+++ src/main/java/com.syntax_highlighters.chess/AccountManager.java
@@ -163,10 +163,10 @@ public class AccountManager {
     public void updateRating(Account winner, Account loser){
         winner.addPoints(loser.getRating() + 400);
         loser.addPoints(winner.getRating()- 400);
-        int winnerPoints = winner.getPoints() / (winner.getLossCount() + winner.getWinCount());
-        int loserPoints = loser.getPoints() / (loser.getLossCount() + loser.getWinCount());
-        winner.setRating(winnerPoints);
-        loser.setRating(loserPoints);
+        int winnerRating = winner.getPoints() / (winner.getLossCount() + winner.getWinCount()+1);
+        int loserRating = loser.getPoints() / (loser.getLossCount() + loser.getWinCount()+1);
+        winner.setRating(winnerRating);
+        loser.setRating(loserRating);
         return;
     }
 }
