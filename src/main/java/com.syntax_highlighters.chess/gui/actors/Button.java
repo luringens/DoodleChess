44625b876257ff2d9603a96f76d26d250056bc6c
theAzack9 <robin@cavachi.be>
2018-04-19 15:35:09 +0200

    Revert "[UI] potential fix for buttons on mac"
    This reverts commit 37a269c66b34130a21ad624cb83f2e3ead1bce04.

--- src/main/java/com.syntax_highlighters.chess/gui/actors/Button.java
+++ src/main/java/com.syntax_highlighters.chess/gui/actors/Button.java
@@ -175,7 +175,7 @@ public class Button extends Actor {
             Gdx.gl.glClearColor(0,0,0,0);
             shader.setUniformf("u_time", (float)Math.random() * 10000.f);
 
-            batch.draw(template, 0, 0, (int)(800 * 1.6f), 800);
+            batch.draw(template, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
             batch.end();
             buffer.end();
