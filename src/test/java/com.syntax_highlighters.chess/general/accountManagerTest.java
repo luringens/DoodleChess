dadf422d05005109f78b8907577449b9654a19b5
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-19 20:17:57 +0200

    [#77] Add test to document accountManager bug
    After running this test, the new test fails. There is also a null pointer
    exception in a different test, for some reason, probably due to recent changes
    to something in the way AccountManager handles databases.

--- src/test/java/com.syntax_highlighters.chess/general/accountManagerTest.java
+++ src/test/java/com.syntax_highlighters.chess/general/accountManagerTest.java
@@ -62,4 +62,28 @@ class accountManagerTest {
         assertEquals(a1.getRating(), a2.getRating());
     }
 
+    @Test
+    void accountManagerUpdatesScoreCorrectlyForSamePlayerWinningTwice() {
+        AccountManager am = new AccountManager();
+        Account a = new Account("Alice"); // initial rating: 1000
+        Account b = new Account("Bob");   // initial rating: 1000
+        am.addAccount(a);
+        am.addAccount(b);
+        am.updateRating(a, b);
+        // a.getRating() = (1000 + 400*1) / 1 = 1400
+        // b.getRating() = (1000 - 400*1) / 1 = 600
+        am.updateRating(a, b);
+        // a.getRating() = ((1000 + 600) + 400*2) / 2 = 1200
+        //                           ^ new rating of opponent
+        // b.getRating() = ((1000 + 1400) - 400*2) / 2 = 800
+        //                           ^ new rating of opponent
+        assertEquals(1200, a.getRating());
+        assertEquals(800, b.getRating());
+        // This is based on the formula from Wikipedia:
+        // rating = (<sum of opponent ratings> + 400*(wins - losses)) / <games played>
+        // https://en.wikipedia.org/wiki/Elo_rating_system#Performance_rating
+    }
+
+    // TODO: add test to ensure draws don't affect the score wrong
+    // TODO: add tests to ensure unrated wins/losses don't affect the score wrong
 }
