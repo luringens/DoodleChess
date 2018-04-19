139047f16bf29a173adbc7d802ac221974b7994d
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-19 19:26:43 +0200

    [#87] Add scribbles to AbstractScreen

--- src/main/java/com.syntax_highlighters.chess/gui/AbstractScreen.java
+++ src/main/java/com.syntax_highlighters.chess/gui/AbstractScreen.java
@@ -5,17 +5,45 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.syntax_highlighters.chess.ChessGame;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.assets.AssetManager;
+import com.syntax_highlighters.chess.gui.actors.DoodleActor;
 
 /**
  * Abstract screen implementing common functionality among different screens.
- *
- * SUGGESTION: Refactor unused methods of extending classes into this abstract
- * class. Also, the dispose method, which seems to do the same thing for all of
- * them.
  */
 public abstract class AbstractScreen implements Screen {
     private final ChessGame game;
     protected final Stage stage;
+    private final static int N_DOODLES_PER_SCREEN = 10;
+    private final static String[] DOODLES = new String[]{
+        "Doodle/eye.png",
+        "Doodle/chessstars.png",
+        "Doodle/food.png",
+        "Doodle/Coffe.png",
+        "Doodle/dragonball.png",
+        "Doodle/Lhand.png",
+        "Doodle/monkey.png",
+        "Doodle/poop.png",
+        "Doodle/smiley.png",
+        "Doodle/TV.png",
+        "Doodle/halloween.png",
+        "Doodle/earth.png",
+        "Doodle/dango.png",
+        "Doodle/basketball.png",
+        "Doodle/crowd.png",
+        "Doodle/car.png",
+        "Doodle/milk.png",
+        "Doodle/fish.png",
+        "Doodle/wheel.png",
+        "Doodle/UFO.png",
+        "Doodle/pawn.png",
+        "Doodle/Cartman.png",
+        "Doodle/water.png",
+        "Doodle/batman.png",
+        "Doodle/pear.png",
+        "Doodle/Horse.png"
+    };
 
     /**
      * Create a screen based on a ChessGame.
@@ -26,6 +54,15 @@ public abstract class AbstractScreen implements Screen {
     {
         this.game = game;
         this.stage = new Stage(new ScreenViewport());
+        AssetManager assetManager = game.getAssetManager();
+
+        // Draw some random doodles
+        for (int i = 0; i < N_DOODLES_PER_SCREEN; i++) {
+            // select a random doodle
+            int index = (int)(Math.random()*DOODLES.length);
+            Texture doodle = assetManager.get(DOODLES[index], Texture.class);
+            stage.addActor(new DoodleActor(doodle, (i+1)*(float)(2*Math.PI/N_DOODLES_PER_SCREEN)));
+        }
     }
 
     /**
@@ -37,6 +74,18 @@ public abstract class AbstractScreen implements Screen {
     {
         return game;
     }
+
+//    /**
+//     * Render all the actors on the stage.
+//     *
+//     * Subclasses must call this super method the last thing they do in their
+//     * render method.
+//     */
+//    @Override
+//    public void render(float delta) {
+//        stage.act(delta);
+//        stage.draw();
+//    }
     
     /**
      * Disposes classes that needs disposing.
