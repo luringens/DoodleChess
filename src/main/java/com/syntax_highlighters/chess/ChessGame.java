90e4d59dce5be76ea3fe54bee3a2891bce9691f0
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-25 16:59:53 +0200

    [ChessGame] remove duplicate fields and methods
    Removed functionality which ChessGame can simply inherit from AbstractChessGame.

--- src/main/java/com/syntax_highlighters/chess/ChessGame.java
+++ src/main/java/com/syntax_highlighters/chess/ChessGame.java
@@ -14,66 +14,6 @@ import com.syntax_highlighters.chess.entities.*;
  * the game.
  */
 public class ChessGame extends AbstractGame {
-    private final Board board;
-    private IAiPlayer whiteAI = null;
-    private IAiPlayer blackAI = null;
-    private Color nextPlayerColor = Color.WHITE;
-    private List<String> moveHistory = new ArrayList<>();
-    private boolean uiHasForceEndedGame = false;
-
-    /**
-     * Return a List of String containing all moves to date.
-     *
-     * @return A list of strings containing all moves.
-     */
-    public List<String> getMoveHistory(){
-        return moveHistory;
-    }
-
-    private void addMoveToHistory(Move m)
-    {
-
-        String piece = String.valueOf((moveHistory.size()/2)+1) + ". ";
-        if(board.getAtPosition(m.newPos).getColor().isWhite()){
-            piece += "WHITE ";
-        }
-        else{
-            piece += "BLACK ";
-        }
-        piece += m.getPieceString(board) + ": ";
-        int a = m.getPosition().getX();
-        switch(a){
-            case 1:
-                piece += "A";
-                break;
-            case 2:
-                piece += "B";
-                break;
-            case 3:
-                piece += "C";
-                break;
-            case 4:
-                piece += "D";
-                break;
-            case 5:
-                piece += "E";
-                break;
-            case 6:
-                piece += "F";
-                break;
-            case 7:
-                piece += "G";
-                break;
-            case 8:
-                piece += "H";
-                break;
-            default:
-                piece += "ERROR";
-                break;
-        }
-        piece += String.valueOf(m.getPosition().getY());
-        moveHistory.add(piece);
-    }
 
     /**
      * Creates a new, blank game and sets up the board.
@@ -110,132 +50,6 @@ public class ChessGame extends AbstractGame {
     }
 
     /**
-     * Validate and perform a move.
-     * @param from Coordinate from
-     * @param to Coordinate to
-     * @return If the move was valid and performed.
-     */
-    public List<Move> performMove(Position from, Position to) {
-        IChessPiece piece = getPieceAtPosition(from);
-        
-        // Check that the piece exists
-        if (piece == null) return new ArrayList<>(); // there is no piece at the given position
-        
-        // Check that the piece belongs to the current player
-        if (piece.getColor() != nextPlayerColor) return new ArrayList<>(); // wrong color of piece
-        
-        // Performs move if valid, returns whether move was performed
-        List<Move> result = board.getMove(piece, to);
-        if (result.size() == 1) {
-            Move move = result.get(0);
-            performMove(move);
-        }
-        return result;
-    }
-
-    /**
-     * Perform the move.
-     *
-     * NOTE: no validation, such as in performMove - the game assumes that the
-     * move is valid, since it should have been retrieved using a previous call
-     * to performMove - the caller is responsible for ensuring that they do not
-     * call this move with an argument that is invalid.
-     *
-     * @param m The move to be performed
-     */
-    public void performMove(Move m) {
-        assert m != null;
-        m.DoMove(board);
-        board.setLastMove(m);
-        addMoveToHistory(m);
-        nextPlayerColor = nextPlayerColor.opponentColor();
-    }
-
-    /**
-     * Perform AI move if next player is AI.
-     *
-     * If the next player is an AI player, make the player perform a move, and
-     * then change turns. Otherwise do nothing.
-     *
-     * @return The move that was performed or null if no move was performed.
-     */
-    public Move PerformAIMove() {
-        if (nextPlayerIsAI()) {
-            Move move;
-            if (nextPlayerColor.isWhite()) {
-                move = whiteAI.GetMove(board);
-            }
-            else {
-                move = blackAI.GetMove(board);
-            }
-            this.performMove(move);
-            return move;
-        }
-        return null;
-    }
-
-    /**
-     * Check whether the next player is AI player or human player.
-     *
-     * @return true if the next player is an AI, false if the next player is a
-     * human
-     */
-    public boolean nextPlayerIsAI() {
-        return (nextPlayerColor.isWhite() && whiteAI != null)
-                || (nextPlayerColor.isBlack() && blackAI != null);
-    }
-
-    /**
-     * Get the board.
-     *
-     * NOTE: Returns a reference to the internal Board in this class. Changes
-     * made to the return value affects the original.
-     *
-     * @return The current board state
-     */
-    public Board getBoard() {
-        return this.board;
-    }
-
-    /**
-     * Check whether a Position is on the board.
-     *
-     * @param p The position to check
-     * @return true if the position is on the board, false otherwise
-     */
-    public boolean isOnBoard(Position p) {
-        return board.isOnBoard(p);
-    }
-
-    /**
-     * Get the piece currently occupying position p
-     *
-     * @param p The position to retrieve piece at
-     * @return null if position is unoccupied, the piece occupying it otherwise
-     */
-    public IChessPiece getPieceAtPosition(Position p) {
-        return this.board.getAtPosition(p);
-    }
-
-    /**
-     * Get all pieces on board.
-     *
-     * @return A list of all the pieces currently on the board
-     */
-    public List<IChessPiece> getPieces() {
-        return this.board.getAllPieces();
-    }
-
-    /**
-     * Check whose turn it is next.
-     *
-     * @return true if it's white's turn to move, false otherwise
-     */
-    public Color nextPlayerColor() {
-        return nextPlayerColor;
-    }
-
-    /**
      * Checks if game is over.
      *
      * Deliberately ignores fifty-move rule and threefold repetition (at least
@@ -243,8 +57,9 @@ public class ChessGame extends AbstractGame {
      *
      * @return true if the game is over, false otherwise
      */
+    @Override
     public boolean isGameOver() {
-        return uiHasForceEndedGame 
+        return super.isGameOver()
                 || board.checkMate(Color.WHITE)
                 || board.checkMate(Color.BLACK)
                 || insufficientMaterial()
@@ -297,17 +112,6 @@ public class ChessGame extends AbstractGame {
     }
 
     /**
-     * Determine whether two squares are the same color.
-     *
-     * @param p1 A position to compare.
-     * @param p2 A position to compare.
-     * @return true if the squares are the same color, false otherwise
-     */
-    public boolean sameColoredSquare(Position p1, Position p2) {
-        return (p1.getX() + p1.getY()) % 2 == (p2.getX() + p2.getY()) % 2;
-    }
-
-    /**
      * Determine the winner of the game, if any.
      *
      * Only determines winner by checkmate (not by resignation).
@@ -315,19 +119,10 @@ public class ChessGame extends AbstractGame {
      * @return 1 if white wins, -1 if black wins, or 0 if the game is not over
      * or a draw
      */
+    @Override
     public int getWinner() {
         if (board.checkMate(Color.WHITE)) return -1;
         if (board.checkMate(Color.BLACK)) return 1;
         return 0;
     }
-
-    /**
-     * Force the game to end.
-     *
-     * This is called by the UI when a player gives up.
-     */
-    public void forceGameEnd() {
-        uiHasForceEndedGame = true;
-    }
-
 }
