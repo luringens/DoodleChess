0c0a06e37a3c903724c36025f237c7fff6fb3ccd
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-26 14:59:07 +0200

    [SjadamGame] add functionality for adding SjadamMoves :)

--- src/main/java/com/syntax_highlighters/chess/SjadamGame.java
+++ src/main/java/com/syntax_highlighters/chess/SjadamGame.java
@@ -2,6 +2,7 @@ package com.syntax_highlighters.chess;
 
 import java.util.List;
 import com.syntax_highlighters.chess.entities.IChessPiece;
+import java.util.ArrayList;
 
 /**
  * Custom game mode with sjadam rules.
@@ -23,7 +24,32 @@ public class SjadamGame extends AbstractGame {
     @Override
     public List<Move> allPossibleMoves() {
         // TODO: implement
+
+        List<Move> allMoves = new ArrayList<>();
+        List<IChessPiece> pieces = getPieces();
+
+        for (IChessPiece piece : pieces) {
+            tryAddSjadamMove(piece, p -> p.south(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.north(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.west(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.east(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.northwest(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.northeast(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.southwest(1), allMoves);
+            tryAddSjadamMove(piece, p -> p.southeast(1), allMoves);
+        }
         return null;
+    }
+
+    /**
+     * Helper method: add move jumping over piece if possible.
+     */
+    private void tryAddSjadamMove(IChessPiece piece, Position.Manipulator dir, List<Move> list) {
+        Position p = piece.getPosition();
+        Position next = dir.transform(p);
+        Position next2 = dir.transform(next);
+        if (board.isOccupied(next) && ! board.isOccupied(next2))
+            list.add(new Move(p, next2, board));
     }
 
     /**
